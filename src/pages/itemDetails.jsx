4ffc9dece6219fa7fd6d48eb858709188a8774b4
MaYa <armanalam2410@gmail.com>
2026-02-05 13:44:58 +0600

    responsive update

--- src/pages/itemDetails.jsx
+++ src/pages/itemDetails.jsx
@@ -69,11 +69,12 @@ const ItemDetails = ()=> {
       })
     }
     return (
-        <div className="w-11/12 mx-auto my-10 rounded-2xl card card-side shadow-sm bg-[#1E3A8A90]">
-          <figure>
+        <div className="w-11/12 mx-auto my-10 rounded-2xl card card-side flex flex-col sm:flex-row shadow-sm bg-[#1E3A8A90]">
+          <figure className='sm:w-6/13'>
             <img
               src={thumbnail}
-              alt="Item Image" />
+              alt="Item Image" 
+              className='rounded-xl sm:rounded-none object-fill'/>
           </figure>
           <div className="card-body">
             <h2 className="text-2xl font-semibold border-dotted border-b-3 py-2">{postType} Item Details</h2>
